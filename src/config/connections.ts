63ac60b3768a549e65561e788f798d48e4ccf7d6
Kbasius <kbasius@gmail.com>
2024-12-29 09:03:50 -0500

    User Stuff and notes
    Created userController.ts as well as userModel.ts
    
    Added notes to many things

--- src/config/connections.ts
+++ src/config/connections.ts
@@ -1,18 +1,26 @@
+// this imports the mongoose library so we can do things to a mongo database
 import mongoose from 'mongoose';
+// this imports the dotenv library, which is how you will basically "log in" with a username and password to a database, If applicable
 import dotenv from 'dotenv';
 
+// calls config method on dotenv, takes info in .env file if applicable to connect to database
 dotenv.config();
 
+// this is an asynchornous function called connectDB, this is the function that will actually connect to the database
 const connectDB = async () => {
+// Try and connect to the mongo data base with info from .env if applicable
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI as string, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
+// connection to the database is successful
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+// catches any errors
   } catch (error) {
+// connection to the data was not successful, and logs why
     console.error(`Error: ${error.message}`);
+// this will crudley end the process if an error happens
     process.exit(1);
   }
 };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
+// imports the express framework
 import express from 'express';
+// this imports the connectDB() function created in connections.ts
 import connectDB from './config/connections';
 
+// an instance of express application is created and assigned to the variable app
 const app = express();
 
+// this calls the connectDB function we imported above
 connectDB();
 
-const PORT = ProcessingInstruction.env.PORT || 3001;
+// setting the variable PORT to either what the .env file says or 3001 if nothing is there
+const PORT = process.env.PORT || 3001;
+// this starts the express server and waits for incoming requests on the port listed above
 app.listen(PORT, () => {
+// if the the server is good and successfully waiting for incoming requests the message below is logged to the console
     console.log(`Server is running on port ${PORT}`);
 });
 
